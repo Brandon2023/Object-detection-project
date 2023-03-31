38c77a3c44e6b1d4497e17c54660f3714e5fd0e6
Brandon <98434819+Brandon2023@users.noreply.github.com>
2023-03-30 21:25:08 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,29 +1,40 @@
 import cv2 as cv
 
 cam = cv.VideoCapture(0)
-package_data = cv.CascadeClassifier('haarcascade_eye.xml.xml')
+eye_data = cv.CascadeClassifier('haarcascade_eye.xml')
+nose_data = cv.CascadeClassifier('haarcascade_eye.xml')
 
 if not cam.isOpened():
     print("Camera not working")
     exit()
 
-def package(frame):
+def eye(frame):
     frame_colour = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
-    box = package_data.detectMultiScale(frame_colour)
-    for (x,y,w,h) in box:
+    box_eye = eye_data.detectMultiScale(frame_colour)
+    for (x,y,w,h) in box_eye:
         cv.rectangle(frame, (x,y), (x+w,y+h), (0,255,0), 2)
-        cv.putText(frame,'test', (x,y-10, cv.FONT_HERSHEY_COMPLEX_SMALL, 0.8, (0,255,0), 2))
+        cv.putText(frame, 'test', (x, y-10), cv.FONT_HERSHEY_COMPLEX_SMALL, 0.8, (0,255,0), 2)
         print("eyes detected")
 
+def nose(frame):
+    frame_colour = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+    box_nose = nose_data.detectMultiScale(frame_colour)
+    for (x,y,w,h) in box_nose:
+        cv.rectangle(frame, (x,y), (x+w,y+h), (0,0,225), 2)
+        cv.putText(frame, 'test 2', (x, y-10), cv.FONT_HERSHEY_COMPLEX_SMALL, 0.8, (0,255,0), 2)
+        print("nose detected")
+
+       
 while True:
     ret, frame = cam.read()
-    package(frame)
+    eye(frame)
+    nose(frame)
 
     if ret is None:
         print("no stream") 
         break
 
-    cv.imshow('package detector', frame)
+    cv.imshow('eye detector', frame)
     if cv.waitKey(1) == ord('w'):
         break
 
